d9b99d6523317ecd9307ecfeb8b995a5bc1f2761
Ian Daniher <it.daniher@gmail.com>
2014-05-26 21:37:11 -0400

    add testing code, pdf docs

---
+++ test.py
@@ -0,0 +1,74 @@
+import glob, serial, time
+
+s = serial.Serial("/dev/ttyUSB0", 9600)
+
+def setR (addr, value):
+	s.write("\xAA\x55S"+chr(addr)+chr(value>>8)+chr(value&0xff)+"\r\n")
+	s.flush()
+	print s.readline()+s.readline()+s.readline()
+
+def peek (addr):
+	s.write("\xAA\x55E"+chr(addr>>8)+chr(addr&0xff)+"\r\n")
+	s.flush()
+	print s.readline()+s.readline()
+	return eval("0x"+s.readline())
+
+def poke (addr, val):
+	s.write("\xAA\x55O"+chr(addr>>8)+chr(addr&0xff)+chr(val&0xff)+"\r\n")
+	s.flush()
+	print s.readline()+s.readline()
+	return eval("0x"+s.readline())
+
+def setRf (freq):
+	x = int(freq*8000)
+	setR(0x29, x>>16)
+	setR(0x2a, x&0xffff)
+
+
+init = [
+ ( 0x30, 0x0001 ),
+ ( 0x30, 0x0004 ),
+ ( 0x04, 0x0FD0 ),
+ ( 0x0B, 0x1A10 ),
+ ( 0x2B, 0x32C8 ),
+ ( 0x2C, 0x1964 ),
+ ( 0x32, 0x627C ),
+ ( 0x33, 0x0AF2 ),
+ ( 0x47, 0x2C2F ),
+ ( 0x4E, 0x293A ),
+ ( 0x54, 0x1D4C ),
+ ( 0x56, 0x0652 ),
+ ( 0x6E, 0x062D ),
+ ( 0x70, 0x1029 ),
+ ( 0x7F, 0x0001 ),
+ ( 0x05, 0x001F ),
+ ( 0x7F, 0x0000 ),
+ ( 0x30, 0x3006 ),
+ ( 0x0A, 0x07FF ), #PA Bias 0000 0100 '00 00' 0000
+ ( 0x1F, 0x0000 ), #GPIO selection 0001 1110 1011 1001
+ ( 0x30, 0x3006 ),
+ ( 0x0F, 0x6b24 ),
+ ( 0x48, 0x03FF ), #0000 0011 1111 0000
+ ( 0x49, 0x01b3 ), #0003
+ ( 0x3C, 0x0958 ), #0000 1001 0101 1000
+ ( 0x43, 0xFFFF ),
+ ( 0x30, 0x3006 ),
+ ( 0x41, 0b0000000001000000),
+ ( 0x42, 0b0000000001000000),
+ ( 0x58, 0b111111)
+]
+
+
+
+[setR(x,y) for (x,y) in init]
+poke(0x45, 0b0101)
+setRf(433.920)
+
+[setR(x,y) for (x,y) in [(0x3c, 0x958), (0x1f, 0xc000), (0x30, 0x3046|1<<4)]]
+
+poke(0xc2, peek(0xc2)|1)
+poke(0xc3, peek(0xc3)|1)
+raw_input()
+poke(0xc2, peek(0xc2)^1)
+
+[setR(x,y) for (x,y) in [(0x1f, 0x0), (0x30, 0x302E)]]
